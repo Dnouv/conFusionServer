a61bea4735d79ec4e45134812d1cf89c354129f1
Dnouv <sdevanshu90@yahoo.com>
2021-06-01 21:21:38 +0530

    Basic Authentication

--- app.js
+++ app.js
@@ -29,6 +29,35 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+const auth = (req, res, next) => {
+  console.log(req.headers)
+
+  var authHeader = req.headers.authorization
+  if(!authHeader) {
+    var err = new Error('You are not authenticated!')
+    res.setHeader('WWW-Authenticate', 'Basic')
+    err.status = 401;
+    return next(err)
+  }
+
+  var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':')
+  let username = auth[0]
+  let password = auth[1]
+
+  if(username === 'admin' && password === 'password') {
+    next()
+    console.error("Auth is",auth)
+  }
+  else {
+    var err = new Error('You are not authenticated!')
+    res.setHeader('WWW-Authenticate', 'Basic')
+    err.status = 401;
+    return next(err)
+  }
+}
+
+app.use(auth)
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
